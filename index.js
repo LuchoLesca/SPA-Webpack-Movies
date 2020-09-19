36decb49068cbc7c8a6d044517188da107be5282
Lucho Lesca <lescaluciano@hotmail.com>
2020-09-19 02:38:05 -0300

    Css separados. Antes de probar plugins y loaders para trabajar mejor con css

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import './src/styles/styles.css';
+//import './src/styles/styles.css';
+
+import './src/styles/global-styles.css';
+import './src/styles/header.css';
+import './src/styles/main.css';
+import './src/styles/home.css';
+import './src/styles/film.css';
+import './src/styles/error404.css';
+
 import { loadIndex, router } from './src/routing/routing';
 
 window.addEventListener('load', () => {
